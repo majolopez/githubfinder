c0ee6eb6e04b5c18456f3a147b83a6c19d0b63a0
Maria Lopez <majolopez1991@gmail.com>
2021-12-12 11:07:54 -0500

    upload

---
+++ src/components/users/Search.js
@@ -0,0 +1,42 @@
+import React, { useContext, useState } from 'react';
+import PropTypes from 'prop-types';
+import GithubContext from '../../context/github/githubContext';
+import AlertContext from '../../context/alert/aletContext';
+
+
+const Search = ({setAlert}) => {
+  const githubContext = useContext(GithubContext);
+  const alertContext = useContext(AlertContext);
+
+  const[text, setText] = useState(''); 
+
+
+  const onChange = (e) => setText(e.target.value);
+
+  const onSubmit = (e) =>{
+    e.preventDefault();
+
+    if(text === ''){
+      alertContext.setAlert('Please enter something', 'light');
+    } else {
+      githubContext.searchUsers(text);
+      setText('');
+    }
+
+  }
+
+  return (
+    <div>
+      <form onSubmit={onSubmit} className="form">
+        <input type="text" name="text" placeholder="Search Users..." value={text} onChange={onChange}/>
+        <input type="submit" value="Search" className="btn btn-dark btn-block"/>
+      </form> {githubContext.users.length > 0 &&<button className="btn tbn-light btn-block" onClick={githubContext.clearUsers}>Clear</button>}
+    </div>
+  )
+}
+
+Search.propTypes = {
+  setAlert: PropTypes.func.isRequired,
+}
+
+export default Search
